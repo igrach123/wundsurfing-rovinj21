ff606692a5c838f48df2b29c4e093fc00ec8cb0b
Igor <brajkovich.igor@gmail.com>
2021-05-13 17:32:00 +0200

    updated with filezila

---
+++ php/video.php
@@ -0,0 +1,7 @@
+<section class="container-fluid" id="video-bg">
+<div class="embed-responsive embed-responsive-16by9">
+    <video autoplay="" loop="" class="embed-responsive-item" controls muted>
+        <source src="video/video-bg.mp4" type="video/mp4">
+    </video>
+</div>
+</section>
